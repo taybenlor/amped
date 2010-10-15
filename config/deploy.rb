dc066129b1f0ef83b9cd581a2fae44021f155b20
Ben Taylor <xachro@gmail.com>
2010-10-16 00:46:21 +1100

    Fixing database config and adding capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+default_run_options[:pty] = true
+
+require 'bundler/capistrano'
+
+set :application, "nybles"
+set :repository,  "git@github.com:xachro/amped.git"
+set :scm, :git
+
+set :user, "nybles"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :rails_env, "production"
+set :application, "nybles"
+set :deploy_to, "/home/nybles/app"
+set :ssh_options, { :forward_agent => true }
+server "173.203.207.104", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  desc "send the restart signal to the nybles unicorn process"
+  task :restart, :roles => :app do
+    run "kill -HUP `cat /home/nybles/app/shared/pids/unicorn.pid`"
+  end
+end
