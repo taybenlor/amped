33b7ce969cdddcabf68a085a212956a1cd1e4fca
Will Cannings <me@willcannings.com>
2010-10-16 17:00:24 +1100

    Take that Google, now we use the Google Charts API too. Trending power graph over time

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,10 +2,20 @@ module ProductsHelper
   def trending_products_by_tag
     products = Product.trending(100)
     clusters = Hash.new([])
-    
     products.each do |product|
       clusters[product.tags.first.tag] += [product]
     end    
     clusters
+  end
+  
+  def trending_power_graph(product)
+    hour = Time.zone.now.to_i / (60 * 60)
+    series = []    
+    product.created_hour.upto(hour).each do |hour|
+      likes = Like.where("product_id = ? and created_at <= ?", product.id, Time.at(hour * 60 * 60)).count
+      power = likes / (((hour - product.created_hour) + 2) ** 1.8)
+      series << power
+    end
+    "http://chart.apis.google.com/chart?cht=lc&chs=300x225&chds=#{series.min},#{series.max}&chd=t:#{series.join(',')}"
   end
 end
