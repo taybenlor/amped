a02117579be665271f1e49ac20ac10a3663ecabb
Ben Taylor <xachro@gmail.com>
2010-10-16 18:01:00 +1100

    lolol

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,9 @@
 class CommentsController < ApplicationController  
   def create
     @comment = Comment.new(params[:comment])
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully created.') }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-      end
-    end
+    @comment.product_id = params[:product_id]
+    @comment.user = current_user
+    @comment.save
+    redirect_to :back
   end
 end
