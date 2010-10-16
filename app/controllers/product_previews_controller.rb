2468e7a9d60be74a446eb9c615c1b9d5e02ea8f4
James <jamsii@gmail.com>
2010-10-16 16:09:17 +1100

    fix bug with adding previews

--- app/controllers/product_previews_controller.rb
+++ app/controllers/product_previews_controller.rb
@@ -4,7 +4,7 @@ class ProductPreviewsController < ApplicationController
     @preview = ProductPreview.new(params[:product_preview])
     @preview.product_id = params[:product_id]
     @preview.save    
-    redirect_to '/products/1'
+    redirect_to '/products/' + params[:product_id].to_s
   end
   
 end
